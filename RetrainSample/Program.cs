e5ec4e6578422c8e681b13e55dc87d05b2e0c6d9
luisquintanilla <Luis.Quintanilla@microsoft.com>
2021-07-27 09:50:16 -0400

    first commit

---
+++ RetrainSample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.ML;
+
+namespace RetrainSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mlContext = new MLContext();
+            var data = mlContext.Data.LoadFromTextFile<TaxiModel.ModelInput>("taxi-fare-data.csv",hasHeader:true, separatorChar:',');
+            var retrainedModel = TaxiModel.RetrainPipeline(mlContext, data);
+            mlContext.Model.Save(retrainedModel, data.Schema, "TaxiModel-Retrained.zip");
+        }
+    }
+}
